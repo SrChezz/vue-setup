413e71ef22f2a9169e7467a0e1385863e9a712cd
Chezzby <itzchezvil17@gmail.com>
2024-11-22 02:03:38 -0500

    icon usage example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,8 +12,9 @@
       />
       <h2 class="font-bold text-xl text-green-600">Hello World</h2>
       <p class="font-semibold">
-        Setup de <strong class="text-green-600">Vue</strong> con tailwinds,
-        unplugin components e iconos de iconify :)
+        <i-tabler:info-circle inline class="inline align-[-0.25rem]" /> Setup de
+        <strong class="text-green-600">Vue</strong> con tailwinds, unplugin
+        components e iconos de iconify :)
       </p>
     </div>
   </main>
