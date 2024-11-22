cc5f8e01ebc779059c0ad3ac8a5bc77673572cc8
Chezzby <itzchezvil17@gmail.com>
2024-11-22 01:59:41 -0500

    First commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <main class="flex justify-center items-center min-h-dvh">
+    <div class="text-center">
+      <img
+        alt="Vue logo"
+        src="@/assets/logo.svg"
+        width="125"
+        height="125"
+        class="inline"
+      />
+      <h2 class="font-bold text-xl text-green-600">Hello World</h2>
+      <p class="font-semibold">
+        Setup de <strong class="text-green-600">Vue</strong> con tailwinds,
+        unplugin components e iconos de iconify :)
+      </p>
+    </div>
+  </main>
+</template>
